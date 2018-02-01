b6165b72a3992cc248ecd3f957c58a98c6f1bd56
GaCampioto <gcampioto@gmail.com>
2018-01-31 23:08:46 -0200

    refactor: package names

--- src/main/java/br/com/gabriel/recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/br/com/gabriel/recipeapp/Spring5RecipeAppApplication.java
@@ -1,4 +1,4 @@
-package guru.springframework;
+package br.com.gabriel.recipeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
