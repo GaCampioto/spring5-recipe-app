f1e9a3b3b6800713fbf0a700d722182e5e5f4854
GaCampioto <gcampioto@gmail.com>
2018-02-07 22:41:45 -0200

    add: controller / template

--- src/main/java/br/com/gabriel/recipeapp/controller/IndexController.java
+++ src/main/java/br/com/gabriel/recipeapp/controller/IndexController.java
@@ -1,13 +1,26 @@
 package br.com.gabriel.recipeapp.controller;
 
+import br.com.gabriel.recipeapp.domain.Recipe;
+import br.com.gabriel.recipeapp.service.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
 
+    private RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"", "/", "/index"})
-    public String getIndex() {
+    public String getIndex(Model model) {
+        List<Recipe> allRecipes = recipeService.findAllRecipes();
+        model.addAttribute("recipes", allRecipes);
         return "index";
     }
 }
